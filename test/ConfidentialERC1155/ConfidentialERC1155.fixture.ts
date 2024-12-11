eeaff0d48fa5750101473f5d569dca7f7a16ab2b
Vivek Sahu <viveksahu1762@gmail.com>
2024-12-11 14:26:35 +0530

    Revert "Edu Chain Contractsa"
    This reverts commit bd1be22fd1f7afa0da6c5b70a3d2f7f316c605b0.

--- test/ConfidentialERC1155/ConfidentialERC1155.fixture.ts
+++ test/ConfidentialERC1155/ConfidentialERC1155.fixture.ts
@@ -8,7 +8,7 @@ export async function deployConfidentialERC1155(): Promise<IncoEventContract> {
   const signers = await getSigners();
 
   const erc1155ContractFactory = await ethers.getContractFactory("IncoEventContract");
-  const erc1155Contract = await erc1155ContractFactory.connect(signers.alice).deploy("0x275C42c018E70f993734F1D200f474A97Af9Ff8E");
+  const erc1155Contract = await erc1155ContractFactory.connect(signers.alice).deploy("0xc02C45cf15832791D12f41fCA2920a314bE51df5");
 
   return erc1155Contract;
 }
